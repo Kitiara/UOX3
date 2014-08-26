1d508b475914ee5b1eca1402ead2b8ca74b0ead2
Kitiara <vinas_solamnus1@hotmail.com>
2014-08-27 01:05:26 +0300

    Forgotten changes related with the previous commit
    Signed-off-by: Kitiara <vinas_solamnus1@hotmail.com>

--- src/system/Other/effect.cpp
+++ src/system/Other/effect.cpp
@@ -672,11 +672,11 @@ void cEffects::tempeffect(CChar *source, CChar *dest, UI08 num, UI16 more1, UI16
                     case 6:
                     case 7:
                     case 8:
+                    case 10:
                     case 11:
-                    case 12:
-                    case 18:
-                    case 19:
-                    case 21:
+                    case 14:
+                    case 15:
+                    case 16:
                         reverseEffect(Effect);
                         cwmWorldState->tempEffects.Remove(Effect, true);
                         break;
